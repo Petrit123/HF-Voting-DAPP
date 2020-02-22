4ed0cd9f5df47227ea743dc4c9e7a56b76d71d17
Petrit123 <petrit.krasniqi@students.ittralee.ie>
2020-02-22 00:38:25 +0000

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Please enter your full name to continue</h2>
+
+    <form v-on:submit="validateVoter">
+      <input type="text" v-model="loginData.voterName" placeholder="Full Name">
+      <br>
+      <input type="submit" value = "Take me to the voting form">
+      <br>
+      <br>
+      <span v-if="loginResponse">
+        <b> {{ loginResponse.data}} </b>
+      </span>
+      <br>
+      </form>
+      <br>
+      <p class="copyright">© Petrit Krasniqi 2020 </p>
+    </div>
+</template>
+
+<script>
+export default {
+
+  name: 'response',
+  data() {
+  return {
+   msg: 'Welcome to my Vue.js voting app which leverages Hyperledger Fabric and IBM blockchain platform to ensure correct election results',
+   loginData: {},
+   loginResponse: {
+      data: ""
+   }
+  };
+  },
+
+  methods: {
+  async validateVoter(){
+
+  if(!this.loginData.voterName) {
+        console.log("!thislogin");
+        let response = 'Please enter your name to continue';
+        this.loginResponse.data = response;
+  } else {
+     console.log('Petrit Krasniqi')
+     this.$router.push('VotingForm')
+  }
+}
+ }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/VotingForm.vue
@@ -0,0 +1,83 @@
+<template>
+ <div class="posts">
+  <h1>2020 Student Union Election</h1>
+  <input type="radio" id="one" value="Student1" v-model="picked">
+  <label for="one">Nick</label>
+  <br>
+  <input type="radio" id="two" value="Student2" v-model="picked">
+  <label for="two">Rama</label>
+  <br>
+  <input type="radio" id="two" value="Student3" v-model="picked">
+  <label for="two">Jason</label>
+  <br>
+  <input type="radio" id ="two" value="Student4" v-model="picked">
+  <label for="two">Petrit</label>
+  <br>
+  <br>
+  <span v-if="picked">
+    Picked:
+    <b> {{ picked}} </b>
+  </span>
+  <br>
+  <br>
+  <form v-on:submit="castVote">
+   <input type="text" v-model="input.voterName" placeholder="Full Name">
+   <br>
+   <input type="submit" value ="Submit vote">
+   <br>
+   <br>
+   </form>
+
+   <br>
+   <span v-if="response">
+     <b> {{ response }} </b>
+  </span>
+  <br>
+
+ </div>
+</template>
+
+<script>
+
+export default {
+  name: "response",
+  data() {
+    return {
+      input: {},
+      picked: null,
+      response: null
+    };
+  },
+  methods: {
+    async castVote() {
+
+    console.log("picked ");
+    console.log(this.picked);
+    console.log("voterName: ");
+    console.log(this.input.voterName);
+    this.response = null;
+
+    // Error checking -- making sure that the user has selected a candidate
+    if (this.picked == null) {
+       console.log('this.picked === null')
+
+       let response = "You have to select a candidate to vote for!";
+       this.response = response;
+    } else if (this.input.voterName == undefined) {
+      console.log('this.voterName === undefined')
+
+      let response = "You have to enter your name to cast a vote!";
+      this.response = response;
+
+    } else {
+        let response = `Successfully recorded vote for ${this.picked}. Thanks for doing your part!`;
+
+        this.response = response;
+
+        console.log("cast vote");
+        console.log(this.input);
+    }
+  }
+  }
+};
+</script>
